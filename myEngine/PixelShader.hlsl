88fee9e653647a6f8554b38e3b530995c349b723
Magnus Nyqvist <magnus.nyq@gmail.com>
2018-01-23 18:19:13 +0100

    fixed .gitignore

--- myEngine/PixelShader.hlsl
+++ myEngine/PixelShader.hlsl
@@ -15,7 +15,7 @@ float4 main(PS_IN input) : SV_Target
     float3 lightPos = float3(3, 5, -2);
     float3 lightDir = normalize(lightPos - input.worldPos.xyz);
     float diffuse = dot(lightDir, input.Normal);
-    float3 finalColor = 0.2 * float3(1.0f, 0.85892157f, 0.67459216f) + float3(1.0f, 0.980392f, 0.95686275f) * diffuse * 0.9;
+    float3 finalColor = 0.2 * float3(1.0f, 0.85892157f, 0.67459216f) + float3(1.0f, 0.980392f, 0.95686275f) * diffuse;
 
     return float4(finalColor, 1.0f);
 };
